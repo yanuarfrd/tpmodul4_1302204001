bb23f6ffb0159e3efba282caa25a114be66614d1
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-03-18 14:59:27 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TPMODULL4_1302204001/Program.cs
+++ TPMODULL4_1302204001/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD4_1302204001
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            String nama = "Yapi";
+            HaloGeneric.SapaUser<string>(ref nama);
+        }
+    }
+
+    class HaloGeneric
+    {
+        public static void SapaUser<Y>(ref Y nama)
+        {
+            Console.WriteLine("Halo user " + nama);
+        }
+    }
+}
